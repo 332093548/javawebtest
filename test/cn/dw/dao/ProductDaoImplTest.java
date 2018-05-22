5394b0610850fa4ca82d4baecbe757a8c337d5f8
Gua <332093548@qq.com>
2018-05-22 11:21:51 +0800

    抽取了getById,queryByName(交给框架抽取)
      1: 给baseDaoImpl定义T类型
      2: 父类声明：abstract T getById(id )
      3: 子类负责此方法的实现
      4: this代表的是当前调用的对象而非当前对象
      5: 使用debug调试,测试整个调用流程

--- test/cn/dw/dao/ProductDaoImplTest.java
+++ test/cn/dw/dao/ProductDaoImplTest.java
@@ -13,52 +13,53 @@ import cn.dw.model.Product;
 public class ProductDaoImplTest {
 
 	// static说明当前属性是类属性,只有一份,静态方法只能操作静态属性
-		private static ProductDaoImpl daoImpl = null;
+	private static ProductDaoImpl daoImpl = null;
 
-		@BeforeClass  // 测试用例之前会调用,一般用来初始化资源
-		public static void setUpBeforeClass() throws Exception {
-			System.out.println("setUpBeforeClass.....");
-			daoImpl = new ProductDaoImpl();
-		}
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("setUpBeforeClass.....");
+		daoImpl = new ProductDaoImpl();
+	}
 
-		@AfterClass  // 测试用例之后会调用,一般用来销毁资源
-		public static void tearDownAfterClass() throws Exception {
-			System.out.println("tearDownAfterClass.....");
-			daoImpl = null;
-		}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("tearDownAfterClass.....");
+		daoImpl = null;
+	}
 
-		@Test
-		public void testDelete() {
-			daoImpl.delete(2);
-		}
-
-		@Test
-		public void testUpdate() {
-			fail("Not yet implemented");
-		}
-
-		@Test
-		public void testSave() {
-			System.out.println("save........");
-			Product product = new Product();
-			product.setName("xyz!!!");
-			product.setPrice(3.14);
-			product.setRemark("remark");
-			daoImpl.save(product);
-		}
-		
-		@Test
-		public void testQuery() {
+	@Test
+	public void testQueryByName() {
 		// "%%":代表查询所有记录
-			List<Product> proList = daoImpl.queryByName("");
-			for(Product temp:proList) {
-				System.out.println(temp + "=======>" + temp.toString());
-			}
+		List<Product> proList = daoImpl.queryByName("");
+		for (Product temp : proList) {
+			System.out.println(temp + "=======>" + temp.toString());
 		}
-		@Test
-		public void testGetById() {
-			// "%%":代表查询所有记录
-			System.out.println(daoImpl.getById(1));
-		}
+	}
+	
+	@Test
+	public void testGetById() {
+		// "%%":代表查询所有记录
+		System.out.println(daoImpl.getById(1));
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		System.out.println("save........");
+		Product product = new Product();
+		product.setName("xyz!!!");
+		product.setPrice(3.14);
+		product.setRemark("remark");
+		daoImpl.save(product);
+	}
 
 }
